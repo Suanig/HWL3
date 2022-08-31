b51f102c267c8ecf22e72e2e1c40526837be1617
Suanig <suanig88@gmail.com>
2022-08-31 18:17:28 +0300

    Create HWL3T1

---
+++ HWL3T1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+int modul = Math.Abs(number);
+
+if (modul < 9999)
+{
+    Console.WriteLine("Число  не пятизначное");
+}
+else
+if (modul > 99999)
+{
+    Console.WriteLine("Число  не пятизначное");
+}
+else 
+{
+    var digits = modul.ToString().Select(t=>int.Parse(t.ToString())).ToArray();
+    if (Math.Abs(digits[0]) == Math.Abs(digits[4]) || Math.Abs(digits[1]) == Math.Abs(digits[3])) Console.WriteLine("Число палиндром");
+    else Console.WriteLine("Число  не палиндром");
+}
+
+
