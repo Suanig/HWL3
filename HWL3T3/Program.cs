662dea480d9955c64d010c5e88d48559a14b547b
Suanig <suanig88@gmail.com>
2022-08-31 18:52:48 +0300

    Create HWL3T3

---
+++ HWL3T3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 0)
+{
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+} 
+}
+else
+{
+    for (int i = 0; i > N; i--)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+} 
+}
